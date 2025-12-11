903ec50d1537f8ecf618836accff36006311fdfa
Khoirul Ansori <khoirulansori407@gmail.com>
2025-12-11 19:04:00 +0700

    add title & icon app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.binjou48ce"
+    "revision": "0.4pc867h2tcg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
